92c70f6fbe9755884d56f5138a16a8d518b597fb
Lucas Russo <lerwys@gmail.com>
2018-02-26 14:24:37 -0300

    {vivado-prog.py,svf-serial}: add option to generate .svf from .bit

---
+++ svf-serial-vivado-gen.cmd
@@ -0,0 +1,14 @@
+open_hw
+connect_hw_server
+create_hw_target flash_afcv3
+open_hw_target
+
+execute_hw_svf "${SVF_FILE}"
+
+create_hw_device -part xc7a200t
+set_property PROGRAM.FILE {${BITSTREAM_FILE}} [lindex [get_hw_devices] 0]
+program_hw_devices [lindex [get_hw_devices] 0]
+write_hw_svf "${OUTPUT_SVF_FILE}"
+
+close_hw_target
+exit
--- vivado-prog.py
+++ vivado-prog.py
@@ -14,6 +14,7 @@ parser.add_argument('--vivado', type=str, help='Vivado binary path', default='/o
 parser.add_argument('--host_url', type=str, help='Host URL in format <ip>:<port>', default='localhost:3121')
 parser.add_argument('--bit_to_mcs', action='store_true', help='Generate .mcs from given .bit file and write to FLASH',  default=False)
 parser.add_argument('--mcs_to_svf', type=str, help='Generate .svf from given .mcs file')
+parser.add_argument('--bit_to_svf', type=str, help='Generate .svf from given .bit file')
 parser.add_argument('-r', '--repetitions', type=int, help='Number of times to repeat the configuration proccess', default=1)
 
 args = parser.parse_args()
@@ -40,8 +41,19 @@ if (args.bit and args.mcs_to_svf):
     call([args.vivado, '-mode', 'batch', '-source', 'temp-svf-vivado-gen.cmd'])
     os.remove('temp-svf-vivado-gen.cmd')
 
+#Create SVF file from given BIT
+if (args.bit and args.bit_to_svf):
+    if not (args.svf):
+        print( '\n\nWhen generating SVF from BIT an SVF file must be selected!\n')
+        quit()
+    with open('svf-serial-vivado-gen.cmd','r') as svf_script_template, open('temp-svf-serial-vivado-gen.cmd','w') as svf_script_new:
+        for line in svf_script_template:
+            svf_script_new.write(line.replace('${SVF_FILE}', args.svf).replace('${BITSTREAM_FILE}', args.bit).replace('${OUTPUT_SVF_FILE}', args.bit_to_svf))
+    call([args.vivado, '-mode', 'batch', '-source', 'temp-svf-serial-vivado-gen.cmd'])
+    os.remove('temp-svf-serial-vivado-gen.cmd')
+
 #Write new impact batch command files based on templates
-if (args.svf and not args.mcs_to_svf):
+if (args.svf and not args.mcs_to_svf and not args.bit_to_svf):
     with open('scansta-vivado-cfg.cmd','r') as svf_script_template, open('temp-scansta-vivado.cmd','w') as svf_script_new:
         for line in svf_script_template:
             svf_script_new.write(line.replace('${HOST_URL}', args.host_url).replace('${SVF_FILE}', args.svf))
