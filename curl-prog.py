e36ef5b47cb32d59f0b5a6f2ab4cee4239b0665e
Lucas Russo <lerwys@gmail.com>
2021-06-15 08:19:01 -0300

    curl-prog.py: fix slot target to be crate slot number

--- curl-prog.py
+++ curl-prog.py
@@ -7,7 +7,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Program NAT AMC FPGA using CURL tool')
 parser.add_argument('nsvf', type=str, help='nSVF configuration file to program FPGA (need to use NAT nsvf tool)')
 parser.add_argument('host_ip', type=str, help='Host IP, either hostname or IP')
-parser.add_argument('slot', type=int, help='Crate slot number target (see NAT JSM manual)')
+parser.add_argument('slot', type=int, help='Crate slot number')
 parser.add_argument('--curl_bin', type=str, help='CURL binary if not in PATH', default='curl')
 parser.add_argument('--freq_target', type=int, help='Target frequency for the internal JTAG chain (see NAT JSM manual)', default='9')
 parser.add_argument('--host_user', type=str, help='Host username', default='root')
@@ -21,7 +21,7 @@ call([args.curl_bin,
     '-H',
     '"Content-Type: multipart/form-data"',
     '-F',
-    '"XsvfReqTarget=${SLOT_NSVF}"'.replace('${SLOT_NSVF}', str(args.slot)),
+    '"XsvfReqTarget=${SLOT_NSVF}"'.replace('${SLOT_NSVF}', str(args.slot-1)),
     '-F',
     '"XsvfReqFreq=${FREQ_TARGET}"'.replace('${FREQ_TARGET}', str(args.freq_target)),
     '-F',
