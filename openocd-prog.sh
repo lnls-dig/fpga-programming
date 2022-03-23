e6ed77dd8f62fc74422bb748731396da0feb6a81
Augusto Fraga Giachero <afg@augustofg.net>
2022-03-23 16:13:58 -0300

    openocd-prog.sh: Add support to xvc programming
    Support to the XVC protocol (client) has been implemented as a merge
    request to OpenOCD recently. It seems to work very well from my
    tests. The only thing left to do is figure out how to write bitstream
    data to the external flash via jtagspi and then Vivado will not be
    needed anymore unless you want to use Chipscope/ILA/VIO.
    
    https://review.openocd.org/c/openocd/+/6867

--- openocd-prog.sh
+++ openocd-prog.sh
@@ -1,10 +1,51 @@
 #!/bin/sh
 
 bitstream="$1"
+board="$2"
+interface="$3"
 
-if [ -z "$1" ]; then
-	echo "Usage: $0 bitstream_file"
+if [ -z "$bitstream" -o -z "$board" -o -z "$interface" ]; then
+	echo "Usage: $0 bitstream_file board interface"
 	exit 1
 fi
 
-openocd -f afcv4-ftdi-jtag.cfg -c "init; xc7_program xc7.tap; pld load 0 \"${bitstream}\"; exit; ftdi_set_signal JEN 0"
+if [ "$board" = "afcv3" ]; then
+	scansta_cmd="svf afc-scansta.svf -quiet"
+elif [ "$board" = "afcv4" ]; then
+	scansta_cmd=""
+else
+	echo "Board '${board}' not supported!"
+	exit 1
+fi
+
+if [ "$interface" = "ftdi" ]; then
+	openocd -f afcv4-ftdi-jtag.cfg \
+			-c "init" \
+			-c "xc7_program xc7.tap" \
+			-c "pld load 0 \"${bitstream}\"" \
+			-c "exit" \
+			-c "ftdi_set_signal JEN 0"
+elif [ "$interface" = "xvc" ]; then
+	xvc_host="$4"
+	xvc_port="$5"
+
+	if [ -z "$xvc_host" -o -z "$xvc_port" ]; then
+		echo "Usage: $0 bitstream_file board xvc hostname port"
+		exit 1
+	fi
+
+	openocd -c "adapter driver xvc" \
+			-c "adapter speed 3000" \
+			-c "xvc_host ${xvc_host}" \
+			-c "xvc_port ${xvc_port}" \
+			-c "reset_config none" \
+			-c "jtag newtap xc7 tap -irlen 6 -ignore-version -expected-id 0x03636093" \
+			-c "pld device virtex2 xc7.tap 1" \
+			-c "init" \
+			-c "${scansta_cmd}" \
+			-c "pld load 0 \"${bitstream}\"" \
+			-c "exit"
+else
+	echo "Interface '${interface}' not supported!"
+	exit 1
+fi
